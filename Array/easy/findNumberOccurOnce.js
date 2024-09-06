959336cdea8ffdd63f41bad326ebd9bd0ebf77b5
unknown <vcbodheesh@gmail.com>
2024-09-06 22:47:43 +0530

    array questions practice

--- Array/easy/findNumberOccurOnce.js
+++ Array/easy/findNumberOccurOnce.js
@@ -85,5 +85,41 @@ main();
 // Space Complexity: O(maxElement + 1) where maxElement = the maximum element of the array.
 
 
+/* ------------------ Hashing using the map data structure: ----------------- */
+
+function getSingleElement(arr) {
+    // Size of the array:
+    const n = arr.length;
+
+    // Declare the hashmap.
+    // And hash the given array:
+    const hashmap = new Map();
+    for (let i = 0; i < n; i++) {
+        const num = arr[i];
+        hashmap.set(num, (hashmap.get(num) || 0) + 1);
+    }
+
+    // Find the single element and return the answer:
+    for (const [num, count] of hashmap) {
+        if (count === 1) {
+            return num;
+        }
+    }
+
+    // This line will never execute
+    // if the array contains a single element.
+    return -1;
+}
+
+function main() {
+    const arr = [4, 1, 2, 1, 2];
+    const ans = getSingleElement(arr);
+    console.log("The single element is:", ans);
+}
+
+main();      
+
+
+
 
 
